f36bc307620b10855b3d8df7d2730501731a86e2
Brad Alexander <bradalexander@Brads-MacBook-Pro.local>
2020-07-29 21:49:22 -0500

    login takes you to welcome screen now

--- app.js
+++ app.js
@@ -2,17 +2,40 @@ const express = require('express')
 const path = require('path')
 const redis = require('redis')
 const bcrypt =require('bcrypt')
+const session = require('express-session')
 
 const app = express()
 const client = redis.createClient()
 
+const RedisStore = require('connect-redis')(session)
+
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
+//middleware
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => res.render('index'))
-app.listen(3000, () => console.log('Server ready'))
+app.use(
+    session({
+      store: new RedisStore({ client: client }),
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 36000000, //10 hours, in milliseconds
+        httpOnly: false,
+        secure: false,
+      },
+      secret: 'qwerty123456',
+    })
+  )
+
+app.get('/', (req, res) => {
+    if (req.session.userid) {
+        res.render('dashboard')
+    } else {
+        res.render('login')
+    }
+})
 
 app.post('/', (req, res) => {
     const { username, password } = req.body
@@ -23,9 +46,15 @@ app.post('/', (req, res) => {
         }) 
         return
     }
-    console.log(req.body, username, password)
+
+    const saveSessionAndRenderDashboard = userid => {
+        req.session.userid = userid
+        req.session.save()
+        res.render('dashboard')
+        }
 
     client.hget('users', username, (err, userid) => {
+        
         if (!userid) {
           //user does not exist, signup procedure
           client.incr('userid', async (err, userid) => {
@@ -33,20 +62,25 @@ app.post('/', (req, res) => {
             const saltRounds =10
             const hash = await bcrypt.hash(password, saltRounds)
             client.hset(`user:${userid}`, 'hash', hash, 'username', username)
-          })
+            saveSessionAndRenderDashboard(userid)
+        })
         } else {
             client.hget(`user:${userid}`, 'hash', async (err, hash) => {
                 const result = await bcrypt.compare(password, hash)
                 if (result) {
-                  //password ok
+                  saveSessionAndRenderDashboard(userid)
                 } else {
-                  //wrong password
+                  res.render('error', {
+                      message: 'Incorrect password',
+                  })
+                  return
                 }
               })
             }
         })
-      res.end()
+      
     })
+app.listen(3000, () => console.log('Server ready'))
     
 
     
