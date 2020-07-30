e31fe1d2031a0fab141f87f3d8188de6c624cd99
Brad Alexander <bradalexander@Brads-MacBook-Pro.local>
2020-07-29 19:27:12 -0500

    added signup process

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const redis = require('redis')
+const bcrypt =require('bcrypt')
 
+const app = express()
+const client = redis.createClient()
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
@@ -17,9 +20,33 @@ app.post('/', (req, res) => {
     if (!username || !password) {
         res.render('error', {
             message: 'please fill completly'
-        })
+        }) 
         return
     }
     console.log(req.body, username, password)
-    res.end()
-})
+
+    client.hget('users', username, (err, userid) => {
+        if (!userid) {
+          //user does not exist, signup procedure
+          client.incr('userid', async (err, userid) => {
+            client.hset('users', username, userid)
+            const saltRounds =10
+            const hash = await bcrypt.hash(password, saltRounds)
+            client.hset(`user:${userid}`, 'hash', hash, 'username', username)
+          })
+        } else {
+            client.hget(`user:${userid}`, 'hash', async (err, hash) => {
+                const result = await bcrypt.compare(password, hash)
+                if (result) {
+                  //password ok
+                } else {
+                  //wrong password
+                }
+              })
+            }
+        })
+      res.end()
+    })
+    
+
+    
