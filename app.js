a78402711e077787a0337921809a328b3e2fe3e2
Brad Alexander <bradalexander@Brads-MacBook-Pro.local>
2020-07-30 23:15:31 -0500

    working and clickable list of available users to follow. i.e. not self, or already followed

--- app.js
+++ app.js
@@ -29,12 +29,28 @@ app.use(
     })
   )
 
+  // app.get('/', (req, res) => {
+  //   if (req.session.userid) {
+  //     client.hkeys('users', (err, users) => {
+  //       console.log(users)
+  //       res.render('dashboard', {
+  //         users
+  //       })
+  //     })
+  //   } else {
+  //     res.render('login')
+  //   }
+  // })
+
   app.get('/', (req, res) => {
     if (req.session.userid) {
-      client.hkeys('users', (err, users) => {
-        console.log(users)
-        res.render('dashboard', {
-          users
+      client.hget(`user:${req.session.userid}`, 'username', (err, currentUserName) => {
+        client.smembers(`following:${currentUserName}`, (err, following) => {
+          client.hkeys('users', (err, users) => {
+            res.render('dashboard', {
+              users: users.filter((user) => user !== currentUserName && following.indexOf(user) === -1)
+            })
+          })
         })
       })
     } else {
@@ -48,6 +64,22 @@ app.get('/post', (req, res) => {
     } else {
         res.render('login')
     }
+})
+
+app.post('/follow', (req, res) => {
+  if (!req.session.userid) {
+    res.render('login')
+    return
+  }
+
+  const { username } = req.body
+  
+  client.hget(`user:${req.session.userid}`, 'username', (err, currentUserName) => {
+    client.sadd(`following:${currentUserName}`, username)
+    client.sadd(`followers:${username}`, currentUserName)
+  })
+
+  res.redirect('/')
 })
 
 app.post('/post', (req, res) => {
@@ -85,29 +117,28 @@ app.post('/', (req, res) => {
       })
     }
 
-        const handleSignup = (username, password) => {
-            client.incr('userid', async (err, userid) => {
-                client.hset('users', username, userid)
-                const saltRounds =10
-                const hash = await bcrypt.hash(password, saltRounds)
-                client.hset(`user:${userid}`, 'hash', hash, 'username', username)
+      const handleSignup = (username, password) => {
+          client.incr('userid', async (err, userid) => {
+              client.hset('users', username, userid)
+              const saltRounds =10
+              const hash = await bcrypt.hash(password, saltRounds)
+              client.hset(`user:${userid}`, 'hash', hash, 'username', username)
+              saveSessionAndRenderDashboard(userid)
+          })
+      }
+      const handleLogin = (userid, password) => {
+          client.hget(`user:${userid}`, 'hash', async (err, hash) => {
+              const result = await bcrypt.compare(password, hash)
+              if (result) {
                 saveSessionAndRenderDashboard(userid)
+              } else {
+                res.render('error', {
+                    message: 'Incorrect password',
+                })
+                return
+              }
             })
-        }
-
-        const handleLogin = (userid, password) => {
-            client.hget(`user:${userid}`, 'hash', async (err, hash) => {
-                const result = await bcrypt.compare(password, hash)
-                if (result) {
-                  saveSessionAndRenderDashboard(userid)
-                } else {
-                  res.render('error', {
-                      message: 'Incorrect password',
-                  })
-                  return
-                }
-              })
-        }
+      }
 
     client.hget('users', username, (err, userid) => {
         if (!userid) {
